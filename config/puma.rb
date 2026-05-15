7d0037ad798137c3461b3e81bac1b57109158755
Svyatoslav Kryukov <me@skryukov.dev>
2026-05-15 16:46:35 +0300

    Update Docker & SSR setup (#265)
    * Update Docker & SSR setup
    
    * Update deps

--- config/puma.rb
+++ config/puma.rb
@@ -39,6 +39,9 @@ plugin :tmp_restart
 # Run the Solid Queue supervisor inside of Puma for single-server deployments.
 plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 
+# Run the Inertia SSR Node.js process alongside Puma when config.ssr_enabled is true.
+plugin :inertia_ssr
+
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
